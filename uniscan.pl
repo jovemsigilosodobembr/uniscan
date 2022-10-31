a53542d512dfd2e75397632299f6ea202d500595
jovemsigilosodobembr <114019171+jovemsigilosodobembr@users.noreply.github.com>
2022-10-31 15:32:17 -0300

    Update uniscan.pl

--- uniscan.pl
+++ uniscan.pl
@@ -1,24 +1,7 @@
 #!/usr/bin/env perl
 
 #    Uniscan Web Vulnerability Scanner
-#    Copyright (C) 2012  Douglas Poerschke Rocha
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#    Development Team:
-#
-#	 Douglas Poerschke Rocha since version 1.0
-
+#    Copyright (C) 2022  jovemsigilosodobembr
 	
 use FindBin qw( $RealBin );
 use lib $RealBin;
